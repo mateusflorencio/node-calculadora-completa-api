e4acbb6b1f09e76d66d745227a44cfa18c532c73
mateus <mateusflorencio@outlook.com.br>
2022-11-04 15:17:52 -0300

    move sut to a single factory

--- __tests__/src/domain/usecase/calculator.spec.js
+++ __tests__/src/domain/usecase/calculator.spec.js
@@ -1,30 +1,27 @@
-import { expect, it, describe } from '@jest/globals'
+import { expect, it, describe, beforeEach } from '@jest/globals'
 import { Calculator } from '../../../../src/domain/usecase'
 
 describe('Calculator', () => {
+  let sut
+  beforeEach(() => {
+    sut = new Calculator()
+  })
   it('should return explode correct', () => {
-    const sut = new Calculator()
-
     sut.calc('22454*45/8/4')
     expect(sut.eq).toEqual(['22454', '*', '45', '/', '8', '/', '4'])
   })
 
   it('should return explode correct', () => {
-    const sut = new Calculator()
-
     sut.calc('2+(48*8)/4')
     expect(sut.eq).toEqual(['2', '+', '(', '48', '*', '8', ')', '/', '4'])
   })
 
   it('should return explode correct', () => {
-    const sut = new Calculator()
-
     sut.calc('2+((48*8)/4)')
     expect(sut.eq).toEqual(['2', '+', '(', '(', '48', '*', '8', ')', '/', '4', ')'])
   })
 
   it('should return throw if input null', () => {
-    const sut = new Calculator()
     try {
       sut.calc()
     } catch (error) {
