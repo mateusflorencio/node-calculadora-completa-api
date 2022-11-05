67a7f393492c7ac018997cec199f20d821ab54e7
mateus <mateusflorencio@outlook.com.br>
2022-11-05 14:26:58 -0300

    ensure composite working

--- __tests__/domain/usecase/calculadora.spec.js
+++ __tests__/domain/usecase/calculadora.spec.js
@@ -1,23 +1,32 @@
 import { expect, it, describe } from '@jest/globals'
-import { Calculator } from '../../../src/domain/usecase'
+import { Calculator, compositeEquation } from '../../../src/domain/usecase'
+import { sum, sub, div, multi } from '../../../src/domain/model'
 
 describe('Calculator usecase', () => {
+  const composite = {
+    '+': sum,
+    '-': sub,
+    '*': multi,
+    '/': div
+  }
+
+  const sut = Calculator(compositeEquation(composite))
   it('calcule', () => {
-    let out = Calculator(['2', '+', '2'])
+    let out = sut(['2', '+', '2'])
     expect(out).toEqual(4)
-    out = Calculator(['2', '-', '2'])
+    out = sut(['2', '-', '2'])
     expect(out).toEqual(0)
-    out = Calculator(['2', '*', '2'])
+    out = sut(['2', '*', '2'])
     expect(out).toEqual(4)
-    out = Calculator(['2', '/', '2'])
+    out = sut(['2', '/', '2'])
     expect(out).toEqual(1)
-    out = Calculator(['2', '+', '3', '+', '(', '4', '*', '(', '5', '+', '1', ')', ')'])
+    out = sut(['2', '+', '3', '+', '(', '4', '*', '(', '5', '+', '1', ')', ')'])
     expect(out).toEqual(29)
-    out = Calculator(['(', '2', '+', '3', ')', '+', '(', '4', '*', '(', '5', '+', '1', ')', ')'])
+    out = sut(['(', '2', '+', '3', ')', '+', '(', '4', '*', '(', '5', '+', '1', ')', ')'])
     expect(out).toEqual(29)
-    out = Calculator(['(', '2', '+', '3', ')', '+', '(', '(', '4', '*', '8', '*', '8', ')', '*', '2', ')'])
+    out = sut(['(', '2', '+', '3', ')', '+', '(', '(', '4', '*', '8', '*', '8', ')', '*', '2', ')'])
     expect(out).toEqual(517)
-    out = Calculator(['(', '2', '+', '3', ')', '+', '(', '4', '*', '(', '5', '+', '1', '+', '1', '+', '1', ')', ')'])
+    out = sut(['(', '2', '+', '3', ')', '+', '(', '4', '*', '(', '5', '+', '1', '+', '1', '+', '1', ')', ')'])
     expect(out).toEqual(37)
   })
 })
--- src/domain/model/operations.js
+++ src/domain/model/operations.js
@@ -1,4 +1,4 @@
-export const sum = (a, b) => a + b
-export const sub = (a, b) => a - b
-export const multi = (a, b) => a * b
-export const div = (a, b) => a / b
+export const sum = (a, b) => parseInt(a) + parseInt(b)
+export const sub = (a, b) => parseInt(a) - parseInt(b)
+export const multi = (a, b) => parseInt(a) * parseInt(b)
+export const div = (a, b) => parseInt(a) / parseInt(b)
