8b0b068111f869276e6a8bca15108c2adcad6875
mateus <mateusflorencio@outlook.com.br>
2022-11-05 21:27:52 -0300

    fix explode

--- __tests__/domain/usecase/explode.spec.js
+++ __tests__/domain/usecase/explode.spec.js
@@ -2,8 +2,18 @@ import { expect, it, describe } from '@jest/globals'
 import { Explode as sut } from '../../../src/domain/usecase'
 describe('Explode', () => {
   it('should return explode correct', () => {
-    expect(sut('22454*45/8/4')).toEqual(['22454', '*', '45', '/', '8', '/', '4'])
-    expect(sut('22454*(45/8)/4')).toEqual(['22454', '*', '(', '45', '/', '8', ')', '/', '4'])
-    expect(sut('2*45)/8)/4')).toEqual(['2', '*', '45', ')', '/', '8', ')', '/', '4'])
+    let out = sut('22454*45/8/4')
+    expect(out).toEqual(['22454', '*', '45', '/', '8', '/', '4'])
+    out = sut('22454*(45/8)/4')
+    expect(out).toEqual(['22454', '*', '(', '45', '/', '8', ')', '/', '4'])
+    out = sut('2*45)/8)/4')
+    expect(out).toEqual(['2', '*', '45', ')', '/', '8', ')', '/', '4'])
+    out = sut('((2*45)/8)/4')
+    expect(out).toEqual(['(', '(', '2', '*', '45', ')', '/', '8', ')', '/', '4'])
+  })
+
+  it('should return explode correct with parentheses in starting', () => {
+    const out = sut('((2*45)(/8)/4')
+    expect(out).toEqual(['(', '(', '2', '*', '45', ')', '(', '/', '8', ')', '/', '4'])
   })
 })
--- src/domain/usecase/calculator/explode.js
+++ src/domain/usecase/calculator/explode.js
@@ -2,21 +2,29 @@ export function Explode (input) {
   const output = []
   while (input.length > 0) {
     let toCut = 0
-    const numbers = input.match(/\d+/i)[0]
-    if (numbers) {
-      toCut = numbers.length
-      output.push(numbers)
+    if (input[0] === '(') {
+      toCut = noNumbers(input)
+    } else {
+      toCut = numbers(input)
+      toCut += noNumbers(input)
     }
-    const noNumbers = input.match(/\D+/i)
-    if (noNumbers) {
-      toCut += noNumbers[0].length
-      output.push(...noNumbers[0].split(''))
-    }
-
     input = input
       .split('')
       .splice(toCut)
       .join('')
+  }
+  function noNumbers (input) {
+    const noNumbers = input.match(/\D+/i)
+    if (noNumbers) {
+      output.push(...noNumbers[0].split(''))
+      return noNumbers[0].length
+    }
+    return 0
+  }
+  function numbers (input) {
+    const numbers = input.match(/\d+/i)[0]
+    if (numbers) { output.push(numbers) }
+    return numbers.length
   }
   return output
 }
